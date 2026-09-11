dfc975c75843f940212c3b9a839cdf6afb29784c
Neptune668 <2935971352@qq.com>
2026-09-11 19:01:36 +0800

    HelloWorld2

--- module01/src/com/atguigu/java/test/Demo_for.java
+++ module01/src/com/atguigu/java/test/Demo_for.java
@@ -1,4 +1,19 @@
 package com.atguigu.java.test;
 
 public class Demo_for {
+    public static void main(String[] args) {
+        int[] arr = {20, 10, 30, 40, 50};
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j < arr.length-i-1; j++) {
+                if (arr[j+1] > arr[j]){
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
 }
