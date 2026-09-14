201105013a14c688f6ea829a408d631ee5a7c6fc
Neptune668 <2935971352@qq.com>
2026-09-14 09:49:25 +0800

    practice1

---
+++ module01/src/com/atguigu/java/test/day3/practice1/Apple.java
@@ -0,0 +1,5 @@
+package com.atguigu.java.test.day3.practice1;
+
+public class Apple extends Fruit{
+
+}
---
+++ module01/src/com/atguigu/java/test/day3/practice1/Banana.java
@@ -0,0 +1,4 @@
+package com.atguigu.java.test.day3.practice1;
+
+public class Banana extends Fruit{
+}
