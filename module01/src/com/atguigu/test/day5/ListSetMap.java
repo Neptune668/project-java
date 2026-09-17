c455776b06a075885b233d32811f2c83a6a4e8ee
Neptune668 <2935971352@qq.com>
2026-09-17 14:06:58 +0800

    ListSetMap

---
+++ module01/src/com/atguigu/test/day5/ListSetMap.java
@@ -0,0 +1,77 @@
+package com.atguigu.test.day5;
+
+import org.junit.Test;
+
+import java.util.*;
+
+public class ListSetMap {
+    @Test
+    public void testList() {
+        //创建，获取长度，拿元素，遍历
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Tom");
+        list.add("Jerry");
+        list.add("Tom");
+        list.add("Kate");
+        list.add("Bob");
+
+        System.out.println(list.size());
+//        System.out.println(list.get(0));
+        //遍历
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+        for (String s : list) {
+            System.out.println(s);
+        }
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()){
+            String next = iterator.next();
+            System.out.println(next);
+        }
+        list.forEach(s -> System.out.println(s));
+    }
+    @Test
+    public void testSet() {
+        //判断某个元素是否在集合中(比上面多的内容
+        Set<String> set = new HashSet<>();
+        set.add("Tom");
+        set.add("Jerry");
+        set.add("Tom");
+        set.add("Kate");
+        set.add("Bob");
+        System.out.println(set.size());//去重
+        System.out.println(set.contains("Tom"));
+        System.out.println(set.contains("Tom1"));
+        set.forEach(s -> System.out.println(s));
+    }
+    @Test
+    public void testMap() {
+        //遍历不同
+        Map<String, Integer> map = new HashMap();
+        map.put("Tom", 18);
+        map.put("Jerry", 19);
+        map.put("Tom", 20);
+        map.put("Kate", 21);
+        map.put("Bob", 22);
+        System.out.println(map.size());
+        System.out.println(map.get("Tom"));
+        System.out.println(map.get("Tom1"));
+        System.out.println("----------------");
+//        map.forEach((k, v) -> System.out.println(k + ":" + v));
+
+        for (String s : map.keySet()) {
+            System.out.println(s + ":" + map.get(s));
+        }
+        System.out.println("----------------");
+        for (Integer value : map.values()) {
+            System.out.println(value);
+        }
+        System.out.println("----------------");
+        for (Map.Entry<String, Integer> stringIntegerEntry : map.entrySet()) {
+            stringIntegerEntry.getKey();
+            stringIntegerEntry.getValue();
+            System.out.println(stringIntegerEntry.getKey() + ":" + stringIntegerEntry.getValue());
+        }
+    }
+}
