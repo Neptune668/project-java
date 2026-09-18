1b0a42aac390317d082e7fb18720b735da50096e
Neptune668 <2935971352@qq.com>
2026-09-18 19:27:52 +0800

    maven test

---
+++ module03-Maven/src/main/java/com/atguigu/maven/Calculator.java
@@ -0,0 +1,8 @@
+package com.atguigu.maven;
+
+public class Calculator {
+
+    public int sum(int i, int j){
+        return i + j;
+    }
+}
---
+++ module03-Maven/src/main/java/com/atguigu/maven/Good.java
@@ -0,0 +1,9 @@
+package com.atguigu.maven;
+
+public class Good {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+}
