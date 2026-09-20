bc1c1606d5dcc268fa90fdf19d9c8cac73ab3cd3
Neptune668 <2935971352@qq.com>
2026-09-20 15:27:11 +0800

    Tcp

---
+++ module09/src/main/java/com/atguigu/jdbc/MainType.java
@@ -0,0 +1,11 @@
+package com.atguigu.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainType {
+    public static void main(String[] args) {
+        SpringApplication.run(MainType.class, args);
+    }
+}
