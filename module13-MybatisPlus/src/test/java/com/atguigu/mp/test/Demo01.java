636eb851c8e7e6accd42a8d54e1205898ee4ee57
Neptune668 <2935971352@qq.com>
2026-09-24 13:54:35 +0800

    test01

--- module13-MybatisPlus/src/test/java/com/atguigu/mp/test/Demo01.java
+++ module13-MybatisPlus/src/test/java/com/atguigu/mp/test/Demo01.java
@@ -2,6 +2,7 @@ package com.atguigu.mp.test;
 
 import com.atguigu.mp.entity.Employee;
 import com.atguigu.mp.mapper.EmployeeMapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,7 +23,30 @@ public class Demo01 {
     }
     @Test
     public void Test02(){
-        int i = employeeMapper.insert(new Employee(null, "0001", "张三", new BigDecimal(10000), null, null, null));
+        int i = employeeMapper.insert(new Employee(null, "0002", "张三", new BigDecimal(10000), null, null, null));
+        System.out.println(i);
+    }
+    @Test
+    public void Test03(){
+        int i = employeeMapper.deleteById(6);
+        System.out.println(i);
+    }
+    @Test
+    public void Test04(){
+        Employee employee = employeeMapper.selectById(1);
+        System.out.println(employee);
+    }
+    @Test
+    public void Test05(){
+        QueryWrapper<Employee> wrapper = new QueryWrapper<Employee>().eq("emp_name", "沙瑞金");
+        int delete = employeeMapper.delete(null);
+        System.out.println(delete);
+//        int i = employeeMapper.deleteById();
+//        System.out.println(i);
+    }
+    @Test
+    public void Test06(){
+        int i = employeeMapper.deleteById(1);
         System.out.println(i);
     }
 }
